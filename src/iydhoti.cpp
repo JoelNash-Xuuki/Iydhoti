4778bff00fe921ad0e5e50bca8dd48e9061fbfa4
Joe Spilman <jspilman@parallelwireless.com>
2022-04-20 18:50:28 +0100

    Soundin groovy

--- src/iydhoti.cpp
+++ src/iydhoti.cpp
@@ -9,60 +9,72 @@
 
 using namespace std;
 
-class Iydhoti{
-  private:
-	string orc;
-	string sco;
-	vector<string> scos;
-	Csound* sound;
-
-	string readFileIntoString(const string& path) {
-      ifstream input_file(path);
-      if (!input_file.is_open()) {
-        cerr << "Could not open the file - '"
-          << path << "'" << endl;
-       exit(EXIT_FAILURE);
-      }
-      return string((istreambuf_iterator<char>(input_file)), 
-      		 istreambuf_iterator<char>());
+class Iydhoti
+{
+public:
+    Iydhoti()
+    {
+        this->orc = readFileIntoString("src/iydhoti.orc");
+        this->sco = readFileIntoString("src/iydhoti_a.sco");
+        this->scos.push_back(readFileIntoString("src/temp/Bass/Bass_a.sco"));
+        this->scos.push_back(readFileIntoString("src/temp/Snares/Snare_a.sco"));
     }
 
-	inline bool exists_test3 (const string& name) {
-	  struct stat buffer;   
-	  return (stat (name.c_str(), &buffer) == 0); 
-	}
-
-	void setOptions(string s){
-          sound->SetOption("-oaudio.wav");
-          sound->CompileOrc(this->orc.c_str());
-          sound->ReadScore(this->sco.c_str());
-	}
-
-	void play(){
-      sound->Start();
-      sound->Perform();	
-      delete sound;
-	}
-
-  public: 
-	Iydhoti(){
-	  sound = new Csound();
-	  this->orc = readFileIntoString("src/Iydhoti.orc"); 
-	  this->sco = readFileIntoString("src/Iydhoti_a.sco");
-//	  this->scos.push_back(readFileIntoString("src/bass.sco"));
-//	  this->scos.push_back(readFileIntoString("src/snare.sco"));
-	}
-
-	void run(){
-	  //for(int i=0; i < scos.size(); i++){
-	   // this->setOptions(to_string(42));
-	    //this->play();
-	  //}
+    void run()
+    {
+        for(int i = 0; i < scos.size(); i++)
+        {
+            this->setOptions();
+            this->play();
+        }
     }
+
+private:
+    string readFileIntoString(const string& path)
+    {
+        ifstream input_file(path);
+
+        if (!input_file.is_open())
+        {
+            cerr << "Could not open the file - '"
+                << path << "'" << endl;
+            exit(EXIT_FAILURE);
+        }
+
+        return string((istreambuf_iterator<char>(input_file)),
+                       istreambuf_iterator<char>());
+    }
+
+    inline bool exists_test3 (const string& name)
+    {
+        struct stat buffer;
+        return (stat (name.c_str(), &buffer) == 0);
+    }
+
+    void setOptions()
+    {
+        //sound.SetOption("-o audio.wav");
+        sound.SetOption("-+jack_outportname=playback_1");
+        sound.CompileOrc(this->orc.c_str());
+        sound.ReadScore(this->sco.c_str());
+    }
+
+    void play()
+    {
+        sound.Start();
+        sound.Perform();
+    }
+
+    string orc;
+    string sco;
+    vector<string> scos;
+    Csound sound = Csound();
 };
 
-int main(){
-  Iydhoti t = Iydhoti(); 
-  t.run();
-  return 0;
+int main()
+{
+    Iydhoti t = Iydhoti();
+    t.run();
+
+    return 0;
 }
--- src/iydhoti.orc
+++ src/iydhoti.orc
@@ -1,38 +1,72 @@
-sr 	= 		44100
-kr 	= 		4410
-ksmps 	= 		10
-nchnls 	= 		2
+sr 	= 44100
+kr 	= 4410
+ksmps 	= 10
+nchnls 	= 2
 
 	instr   102
-iamp	=	ampdb(p4)
-ipitch	=	cpspch(p5)
-icar	=	p6
-imod	= 	p7
-indx	=	p8
-ifn 	=   p9
+iamp	= ampdb(p4)
+ipitch	= cpspch(p5)
+icar	= p6
+imod	= p7
+indx	= p8
+ifn 	= p9
 kpan	line 	p11, p3, p12
 kenv	oscil  iamp, 1/p3, p10
 a1   	foscil 	iamp*kenv, ipitch, icar, imod, indx, ifn
 	outs     a1 * kpan, a1 *(1-kpan)
 	endin
 
+	instr   103
+iamp	= ampdb(p4)
+ipitch	= cpspch(p5)
+icar	= p6
+imod	= p7
+indx	= p8
+ifn 	= p9
+kpan	line 	p11, p3, p12
+kenv	oscil  iamp, 1/p3, p10
+a1   	foscil 	iamp*kenv, ipitch, icar, imod, indx, ifn
+	outs     a1 * kpan, a1 *(1-kpan)
+	endin
+
+;;	instr	128
+;;idur	= p3
+;;iamp	= p4
+;;ifrq	= p5*1000
+;;iatk	= p6
+;;irel	= p7
+;;icut1	= p8
+;;icut2	= p9
+;;
+;;lPan fmod p2, 1
+;;rPan - 1, lPan
+;;
+;;kpan	line 	lPan, idur, rPan
+;;kenv	linen	iamp, iatk, idur, irel
+;;kcut	expon	icut1, idur, icut2
+;;anoise	rand	ifrq	
+;;afilt	tone	anoise, kcut
+;;       	outs	(afilt*kenv)*kpan, (afilt*kenv)*(1-kpan)  
+;;		dispfft	afilt, idur, 4096
+;;		endin
+
 	instr	129
-idur	=		p3
-iamp	=		p4
-ifrq	=		p5
-iatk	=		p6
-irel	=		p7
-icut1	=		p8
-icut2	=		p9
+idur	= p3
+iamp	= p4
+ifrq	= p5*1000
+iatk	= p6
+irel	= p7
+icut1	= p8
+icut2	= p9
 kpan	line 	p10, idur, p11
 kenv	linen	iamp, iatk, idur, irel
 kcut	expon	icut1, idur, icut2
 anoise	rand	ifrq	
 afilt	tone	anoise, kcut
-       	outs  	(afilt*kenv)*kpan, (afilt*kenv)*(1-kpan)  
+       	outs	(afilt*kenv)*kpan, (afilt*kenv)*(1-kpan)  
 		dispfft	afilt, idur, 4096
 		endin
-		
+
 	instr	130
 idur	=		p3
 iamp	=		p4
